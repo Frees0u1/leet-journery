e88b1918c2ac3078e9f6c29014c20e22a63dc0fa
frees0u1 <loskyer@gmail.com>
2021-05-29 19:14:54 +0800

    solve 4 and 5

--- tool/leet_tool.h
+++ tool/leet_tool.h
@@ -2,29 +2,37 @@
 #include <iostream>
 #include <vector>
 
-namespace leet{
-    void printList(ListNode* l) {
-        while (l != nullptr)
-        {
-            std::cout << l -> val << " -> ";
-            l = l->next;
-        }
-        
-       std::cout << "nullptr" << std::endl;
-    }
-    
-    template <typename T>
-    void printArray(const std::vector<T>& array) {
-        for(auto x : array) {
-            std::cout << x << " ";
-        }
-        std::cout << std::endl;
+namespace leet {
+  void printList(ListNode* l) {
+    while (l != nullptr)
+    {
+      std::cout << l->val << " -> ";
+      l = l->next;
     }
 
-    template <typename T>
-    void printArray(const std::vector<std::vector<T>>& array) {
-        for(auto x : array) {
-            printArray(x);
-        }
+    std::cout << "nullptr" << std::endl;
+  }
+
+  template <typename T>
+  void printArray(const std::vector<T>& array) {
+    for (auto x : array) {
+      std::cout << x << " ";
     }
+    std::cout << std::endl;
+  }
+
+  template <typename T>
+  void printArrayWithIndex(const std::vector<T>& array) {
+    for (int i = 0; i < array.size(); i++) {
+      printf("(%d, %d) ", i, array[i]);
+    }
+    std::cout << std::endl;
+  }
+
+  template <typename T>
+  void printArray(const std::vector<std::vector<T>>& array) {
+    for (auto x : array) {
+      printArray(x);
+    }
+  }
 }
